927f93f08484611d7225e6048c3ec3c72d55e2ba
carloluis <carloluisr@gmail.com>
2018-06-22 13:25:49 -0300

    very simplistic redux implementation

---
+++ redux.js
@@ -0,0 +1,24 @@
+// VERY SIMPLE Redux implementation
+
+function redux(initialState, reducer) {
+	let state = reducer(initialState, { type: '$REDUX_INIT' });
+	let listeners = [];
+
+	return {
+		getState() {
+			return state;
+		},
+		dispatch(action) {
+			state = reducer(state, action);
+			listeners.forEach(listener => listener(state));
+		},
+		subscribe(listener) {
+			listeners.push(listener);
+			return () => {
+				listeners = listeners.filter(l => l !== listener);
+			};
+		}
+	};
+}
+
+module.exports = redux;
