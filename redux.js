a3e63a644ae8281606a60a548226469ed64a769f
carloluis <carloluisr@gmail.com>
2018-06-22 15:21:21 -0300

    add .editorconfig :gear:

--- redux.js
+++ redux.js
@@ -1,24 +1,24 @@
 // VERY SIMPLE Redux implementation
 
 function redux(initialState, reducer) {
-	let state = reducer(initialState, { type: '$REDUX_INIT' });
-	let listeners = [];
+    let state = reducer(initialState, { type: '$REDUX_INIT' });
+    let listeners = [];
 
-	return {
-		getState() {
-			return state;
-		},
-		dispatch(action) {
-			state = reducer(state, action);
-			listeners.forEach(listener => listener(state));
-		},
-		subscribe(listener) {
-			listeners.push(listener);
-			return () => {
-				listeners = listeners.filter(l => l !== listener);
-			};
-		}
-	};
+    return {
+        getState() {
+            return state;
+        },
+        dispatch(action) {
+            state = reducer(state, action);
+            listeners.forEach(listener => listener(state));
+        },
+        subscribe(listener) {
+            listeners.push(listener);
+            return () => {
+                listeners = listeners.filter(l => l !== listener);
+            };
+        }
+    };
 }
 
 module.exports = redux;
